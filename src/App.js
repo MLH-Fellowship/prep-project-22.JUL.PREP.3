c03b268a03b2e7c7d5ebbb8fc35327a5c9874d09
Shehab Adel <shehabeldinadel@gmail.com>
2022-07-19 16:08:40 +0200

    Created two useState hooks to update the weather icon, and background image to match the weather Installed `react-helmet` for updating the `head` tag Modified the App.css to remove the default background color.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import { useEffect, useState } from "react";
 import './App.css';
 import logo from './mlh-prep.png'
-
+import { Helmet } from 'react-helmet'
+import defaultL from './assets/default.jpg'
+import clear from './assets/clear.webp';
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [city, setCity] = useState("New York City")
   const [results, setResults] = useState(null);
 
+
+  const [weatherIcon, setWeatherIcon]=useState(""); //hook for updating the weather icon
+  const [background, setBackground] = useState(defaultL) //default.jpg will be the default background picture in our assets
+
+  const changeBackground = (result) => {
+    const weather = result.weather[0].main
+    const icon = result.weather[0].icon
+    switch (weather) {
+      case "Clear":
+        setBackground(`${clear}`)
+        break;
+
+      default:
+        setBackground(`${defaultL}`)
+        break;
+    }
+    setWeatherIcon(icon);
+  }
   useEffect(() => {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + process.env.REACT_APP_APIKEY)
       .then(res => res.json())
@@ -18,6 +38,9 @@ function App() {
           } else {
             setIsLoaded(true);
             setResults(result);
+            //Inside this function we can make a switch case on results, and change the background picture
+            //to different sources based on the temperature provided
+            changeBackground(result);
           }
         },
         (error) => {
@@ -31,6 +54,10 @@ function App() {
     return <div>Error: {error.message}</div>;
   } else {
     return <>
+      <Helmet>
+        <style>{`body { background-image: url('${background}'); background-repeat: no-repeat;
+  background-size: cover; }`}</style>
+      </Helmet>
       <img className="logo" src={logo} alt="MLH Prep Logo"></img>
       <div>
         <h2>Enter a city below 👇</h2>
@@ -42,7 +69,7 @@ function App() {
           {!isLoaded && <h2>Loading...</h2>}
           {console.log(results)}
           {isLoaded && results && <>
-            <h3>{results.weather[0].main}</h3>
+            <h3>{results.weather[0].main} <span><img src={`http://openweathermap.org/img/wn/${weatherIcon}.png`} alt="Weather Icon"/></span></h3>
             <p>Feels like {results.main.feels_like}°C</p>
             <i><p>{results.name}, {results.sys.country}</p></i>
           </>}
