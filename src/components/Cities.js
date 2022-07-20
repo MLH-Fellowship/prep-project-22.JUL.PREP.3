568a425a0c6190aa5b4b3bcfd9a17185b5de5bf2
LR <liuba.rukhlina@gmail.com>
2022-07-20 19:13:34 +0200

    changed styling of dropdown menu

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -1,12 +1,20 @@
 import React from "react";
 
-export default function Cities({list})
-{
-    return (
-        <ul>
-            {list.map((el,i) => (
-                <li key={i}>{el.name}, {el.country}</li>
-            ))}
-        </ul>
-    )
+export default function Cities({ list }) {
+    if (!list?.length) {
+        return (
+          <div class="no-suggestions">
+            <em>No suggestions, you're on your own!</em>
+          </div>
+        );
+    }
+  return (
+    <ul className="suggestions">
+      {list.map((el, i) => (
+        <li key={i}>
+          {el.name}, {el.country}
+        </li>
+      ))}
+    </ul>
+  );
 }
