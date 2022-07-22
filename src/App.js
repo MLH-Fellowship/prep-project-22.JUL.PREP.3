1736f488f19724617ed3429e13a974602e2bd098
somadisingh <btech10001.19@bitmesra.ac.in>
2022-07-23 00:26:10 +0530

    Resolving App.js conflicts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import logo from "./img/mlh-prep.png";
-import locationIcon from "./img/location-icon.jpg";
+import locationIcon from "./img/location-icon.jpg"
 import ItemCard from "./ItemCard";
 import Objects from "./Utilities/Objects";
 import React from "react";
@@ -13,10 +13,13 @@ import {
   ZoomableGroup,
 } from "react-simple-maps";
 import ReactTooltip from "react-tooltip";
-import { Helmet } from "react-helmet";
-import defaultBg from "./assets/default.jpg";
-import changeBackground from "./utils/changeBackground";
-import Forecast from "./Components/Forecast/Forecast";
+import { Helmet } from 'react-helmet';
+import defaultBg from './assets/default.jpg';
+import changeBackground from './utils/changeBackground';
+import Forecast from "./Components/Forecast/Forecast"
+
+
+
 
 const markers = [
   {
@@ -36,117 +39,118 @@ const geoUrl =
   "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
 
 function App() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [city, setCity] = useState("New York City");
-  const [results, setResults] = useState(null);
-  const [content, setcontent] = useState("");
-  const [objects, setObjects] = useState([]);
-  const [isUseCurrentLocation, setIsUseCurrentLocation] = useState(false);
-  const [latitude, setLatitude] = useState(40.7143);
-  const [longitude, setLongitude] = useState(-74.006);
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [city, setCity] = useState('New York City');
+    const [results, setResults] = useState(null);
+    const [content, setcontent] = useState("");
+    const [objects, setObjects] = useState([]);
+    const [isUseCurrentLocation, setIsUseCurrentLocation] = useState(false);
+    const [latitude, setLatitude] = useState(40.7143);
+    const [longitude, setLongitude] = useState(-74.006);
 
-  const [weatherIcon, setWeatherIcon] = useState(""); //hook for updating the weather icon
-  const [background, setBackground] = useState(defaultBg); //default.jpg will be the default background picture in our assets
+    const [weatherIcon, setWeatherIcon] = useState(''); //hook for updating the weather icon
+    const [background, setBackground] = useState(defaultBg); //default.jpg will be the default background picture in our assets
 
-  const getCurrentPosition = () => {
-    setIsUseCurrentLocation(true);
-    setCity("");
-    const userAllowPositionAccess = (position) => {
-      setLatitude(position.coords.latitude);
-      setLongitude(position.coords.longitude);
+    const getCurrentPosition = () => {
+      setIsUseCurrentLocation(true);
+      setCity("");
+      const userAllowPositionAccess = (position) => {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      };
+
+      const userDenyPositionAccess = (error) => {
+        alert(error.message);
+      };
+
+      window.navigator.geolocation.getCurrentPosition(
+        userAllowPositionAccess,
+        userDenyPositionAccess
+      );
     };
 
-    const userDenyPositionAccess = (error) => {
-      alert(error.message);
-    };
-
-    window.navigator.geolocation.getCurrentPosition(
-      userAllowPositionAccess,
-      userDenyPositionAccess
-    );
-  };
-
-  function bringRightThings(results) {
-    if (results.weather[0].main === "Clear") {
-      setObjects([Objects.hat, , Objects.sunscreen, Objects.sunglasses]);
-    } else if (
-      results.weather[0].main === "Rain" ||
-      results.weather[0].main === "Thunderstorm" ||
-      results.weather[0].main === "Drizzle" ||
-      results.weather[0].main === "Tornado" ||
-      results.weather[0].main === "Squall"
-    ) {
-      setObjects([Objects.raincoat, Objects.umbrella, Objects.boots]);
-    } else if (
-      results.weather[0].main === "Mist" ||
-      results.weather[0].main === "Smoke" ||
-      results.weather[0].main === "Haze" ||
-      results.weather[0].main === "Fog"
-    ) {
-      setObjects([Objects.torch, Objects.Coat]);
-    } else if (results.weather[0].main === "Snow") {
-      setObjects([Objects.coat, Objects.scarf, Objects.boots]);
-    } else if (results.weather[0].main === "Clouds") {
-      setObjects([Objects.coat, Objects.hat]);
-    } else if (
-      results.weather[0].main === "Ash" ||
-      results.weather[0].main === "Dust" ||
-      results.weather[0].main === "Sand"
-    ) {
-      setObjects([Objects.Hat, Objects.Glasses]);
-    }
-  }
-
-  useEffect(() => {
-    let apiURL = "";
-    if (isUseCurrentLocation) {
-      apiURL =
-        "https://api.openweathermap.org/data/2.5/weather?lat=" +
-        latitude +
-        "&lon=" +
-        longitude +
-        "&units=metric&appid=" +
-        process.env.REACT_APP_APIKEY;
-    } else {
-      apiURL =
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        process.env.REACT_APP_APIKEY;
-    }
-
-    const getResults = (result) => {
-      if (result["cod"] !== 200) {
-        setIsLoaded(false);
-      } else {
-        setIsLoaded(true);
-        setResults(result);
-        bringRightThings(result);
-        //Inside this function we can make a switch case on results, and change the background picture
-        //to different sources based on the temperature provided
-        let weatherMetaData = changeBackground(result);
-        setBackground(weatherMetaData.backgroundImg);
-        setWeatherIcon(weatherMetaData.weatherIcon);
+    function bringRightThings(results) {
+      if (results.weather[0].main === "Clear") {
+        setObjects([Objects.hat, , Objects.sunscreen, Objects.sunglasses]);
+      } else if (
+        results.weather[0].main === "Rain" ||
+        results.weather[0].main === "Thunderstorm" ||
+        results.weather[0].main === "Drizzle" ||
+        results.weather[0].main === "Tornado" ||
+        results.weather[0].main === "Squall"
+      ) {
+        setObjects([Objects.raincoat, Objects.umbrella, Objects.boots]);
+      } else if (
+        results.weather[0].main === "Mist" ||
+        results.weather[0].main === "Smoke" ||
+        results.weather[0].main === "Haze" ||
+        results.weather[0].main === "Fog"
+      ) {
+        setObjects([Objects.torch, Objects.Coat]);
+      } else if (results.weather[0].main === "Snow") {
+        setObjects([Objects.coat, Objects.scarf, Objects.boots]);
+      } else if (results.weather[0].main === "Clouds") {
+        setObjects([Objects.coat, Objects.hat]);
+      } else if (
+        results.weather[0].main === "Ash" ||
+        results.weather[0].main === "Dust" ||
+        results.weather[0].main === "Sand"
+      ) {
+        setObjects([Objects.Hat, Objects.Glasses]);
       }
-    };
+    }
 
-    const getError = (error) => {
-      setIsLoaded(true);
-      setError(error);
-    };
+    useEffect(() => {
+      let apiURL = "";
+      if (isUseCurrentLocation) {
+        apiURL =
+          "https://api.openweathermap.org/data/2.5/weather?lat=" +
+          latitude +
+          "&lon=" +
+          longitude +
+          "&units=metric&appid=" +
+          process.env.REACT_APP_APIKEY;
+      } else {
+        apiURL =
+          "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          process.env.REACT_APP_APIKEY;
+      }
 
-    fetch(apiURL)
-      .then((res) => res.json())
-      .then(getResults, getError);
-  }, [city, longitude, latitude, isUseCurrentLocation]);
+      const getResults = (result) => {
+        if (result["cod"] !== 200) {
+          setIsLoaded(false);
+        } else {
+          setIsLoaded(true);
+          setResults(result);
+          bringRightThings(result);
+          isUseCurrentLocation && setCity(result.name)
+          //Inside this function we can make a switch case on results, and change the background picture
+          //to different sources based on the temperature provided
+          let weatherMetaData = changeBackground(result);
+          setBackground(weatherMetaData.backgroundImg);
+          setWeatherIcon(weatherMetaData.weatherIcon);
+        }
+      };
+
+      const getError = (error) => {
+        setIsLoaded(true);
+        setError(error);
+      };
+
+      fetch(apiURL)
+        .then((res) => res.json())
+        .then(getResults, getError);
+    }, [city, longitude, latitude, isUseCurrentLocation]);
 
   if (error) {
     return <div>Error: {error.message}</div>;
   } else {
     return (
-      <>
-        <Helmet>
+      <div className="fade">
+         <Helmet>
           <style>{`body { background-image: url('${background}'); background-repeat: no-repeat;
   background-size: cover; }`}</style>
         </Helmet>
@@ -161,18 +165,12 @@ function App() {
               setIsUseCurrentLocation(false);
             }}
           />
-          <br />
+            <br />
           <button onClick={getCurrentPosition} className="btn">
-            <img
-              className="location-icon"
-              src={locationIcon}
-              alt="Current Location Icon"
-            ></img>{" "}
-            Current Location
+            <img className="location-icon" src={locationIcon} alt="Current Location Icon"></img> Current Location
           </button>
           <div className="Results">
             {!isLoaded && <h2>Loading...</h2>}
-            {console.log(results)}
             {isLoaded && results && (
               <>
                 <h3>{results.weather[0].main}</h3>
@@ -185,57 +183,66 @@ function App() {
               </>
             )}
           </div>
-        </div>
-        <div className="mapContainer">
-          <h1> Global Weather Map </h1>
-          <ReactTooltip>{content}</ReactTooltip>
-          <div style={{ width: "325%", borderStyle: "double" }}>
-            <ComposableMap data-tip="">
-              <ZoomableGroup zoom={1}>
-                {" "}
-                <Geographies geography={geoUrl}>
-                  {({ geographies }) =>
-                    geographies.map((geo) => (
-                      <Geography
-                        key={geo.rsmKey}
-                        geography={geo}
-                        onMouseEnter={() => {
-                          const { name } = geo.properties;
-                          setcontent(`${name}`);
-                          setCity(`${name}`);
-                        }}
-                        onMouseLeave={() => {
-                          setcontent("");
-                          setCity("");
-                        }}
-                        style={{
-                          hover: {
-                            fill: "#F53",
-                            outline: "none",
-                          },
-                        }}
-                      />
-                    ))
-                  }
-                </Geographies>
-                {markers.map(({ name, coordinates, markerOffset }) => (
-                  <Marker key={name} coordinates={coordinates}>
-                    <circle r={10} fill="#F00" stroke="#fff" strokeWidth={2} />
-                    <text
-                      textAnchor="middle"
-                      y={markerOffset}
-                      style={{ fontFamily: "system-ui", fill: "#505A6D" }}
-                    >
-                      {" "}
-                      {name}{" "}
-                    </text>
-                  </Marker>
-                ))}
-              </ZoomableGroup>
-            </ComposableMap>
+          <div
+            className="mapContainer"
+            style={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "centre",
+              alignItems: "center",
+            }}
+          >
+            <h1> Global Weather Map </h1>
+            <ReactTooltip>{content}</ReactTooltip>
+            <div style={{ width: "1400px", borderStyle: "double" }}>
+              <ComposableMap data-tip="">
+                <ZoomableGroup zoom={1}>
+                  {" "}
+                  <Geographies geography={geoUrl}>
+                    {({ geographies }) =>
+                      geographies.map((geo) => (
+                        <Geography
+                          key={geo.rsmKey}
+                          geography={geo}
+                          onMouseEnter={() => {
+                            const { name } = geo.properties;
+                            setcontent(`${name}`);
+                            setCity(`${name}`);
+                          }}
+                          onMouseLeave={() => {
+                            setcontent("");
+                            setCity("");
+                          }}
+                          style={{
+                            hover: {
+                              fill: "#F53",
+                              outline: "none",
+                            },
+                          }}
+                        />
+                      ))
+                    }
+                  </Geographies>
+                  {markers.map(({ name, coordinates, markerOffset }) => (
+                    <Marker key={name} coordinates={coordinates}>
+                      <circle r={10} fill="#F00" stroke="#fff" strokeWidth={2} />
+                      <text
+                        textAnchor="middle"
+                        y={markerOffset}
+                        style={{ fontFamily: "system-ui", fill: "#505A6D" }}
+                      >
+                        {" "}
+                        {name}{" "}
+                      </text>
+                    </Marker>
+                  ))}
+                </ZoomableGroup>
+              </ComposableMap>
+            </div>
           </div>
-        </div>
-        <Forecast />
+          <Forecast />
         <div className="cards">
           {objects &&
             objects.map((object) => {
@@ -251,9 +258,9 @@ function App() {
               );
             })}
         </div>
-      </>
-    );
-  }
-}
+        </div>
+    </div>
+    )
+  }}
 
 export default App;
