8dda64d82e901444b0db721621c5355ff117d9ae
AWilkes <49675331+awilkescom@users.noreply.github.com>
2022-07-25 13:49:59 +0100

    Revert "Merge branch 'main' into development"
    This reverts commit d386104d174ed6d31a20188d8f755280f9b973ff, reversing
    changes made to 8bac92198fcdaa575d0d38094156eeef2d1528c4.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Loader from './Components/initialLoader/initialLoaderComp';
-
-const WeatherApp = ()=>{
-
-  const [isLoading,setIsLoading] = useState(true)
-
-  useEffect(()=>{
-
-     setTimeout(()=>{
-      setIsLoading(false)
-     },3500)
-
-  },[])
-
-  return(<>
-        {isLoading? <Loader/> : <App/>}
-         </>
-  )
-}
 
 ReactDOM.render(
   <React.StrictMode>
-    <WeatherApp />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
