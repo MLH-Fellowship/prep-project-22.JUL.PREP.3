2ba6496d1f08f4d752625895f2b15e5c2bb612b5
codewarsfx <chiderainnocent001@gmail.com>
2022-07-26 15:44:27 +0100

    improvement to result card

---
+++ src/Components/BookmarkIcon/bookmarkIconComponent.jsx
@@ -0,0 +1,22 @@
+import './bookmarkIconStyles.css'
+import {BsBookmarkHeartFill} from 'react-icons/bs'
+import { useState } from 'react'
+
+
+const BookmarkIcon= ()=>{
+const [isClicked,setIsClicked] =useState(false)
+
+
+
+
+return(
+    <>
+     <BsBookmarkHeartFill className="bookmark-icon" style={{color:isClicked?"blue":"#333"}} onClick={()=>setIsClicked(!isClicked)}/>
+     </>
+)
+
+
+}
+
+
+export default BookmarkIcon
