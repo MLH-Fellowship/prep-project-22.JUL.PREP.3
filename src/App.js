0bf8c82fa38c2d1fa6dca3a734b96fe01d67de68
Shehab Adel <shehabeldinadel@gmail.com>
2022-07-28 18:16:20 +0200

    Brought back the Weather Icon next to the degree temperature.

--- src/App.js
+++ src/App.js
@@ -406,7 +406,12 @@ function App() {
               <>
                 <div>
                   <h3>{results.weather[0].main}</h3>
-                  <p>Feels like {results.main.feels_like}°C</p>
+                  <p>
+                    Feels like {results.main.feels_like}°C
+                    <span>
+                      <img src={`http://openweathermap.org/img/wn/${weatherIcon}.png`} alt='Weather Icon'/>
+                    </span>
+                  </p>
                   <i>
                     <p>
                       {results.name}, {results.sys.country}
